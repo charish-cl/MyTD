12f2682754b5a683d00fd4e134049376dfe89f3d
charish-cl <61883438+charish-cl@users.noreply.github.com>
2021-05-04 23:20:23 +0800

    delete

--- Assets/Scripts/TestGit.cs
+++ Assets/Scripts/TestGit.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TestGit : MonoBehaviour
 {
- 
+    private void Start()
+    {
+        Debug.Log("dwadawd");
+    }
 }
